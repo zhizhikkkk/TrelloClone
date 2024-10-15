ea06a0c1826d4fef527a064dce8bd80b39f052ad
Alina Abdullina <fianit8@gmail.com>
2024-10-15 13:53:29 +0000

    removed previous project files

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++
@@ -1,5 +0,0 @@
-const OrganizationIdPage = async () => {
-  return <div className="w-full mb-20"></div>;
-};
-
-export default OrganizationIdPage;
--- lib/org-stub.ts
+++
@@ -1,6 +0,0 @@
-import { setCookie } from "cookies-next";
-
-export const createOrgStub = (orgTitle: string): boolean => {
-  setCookie("currentOrg", orgTitle);
-  return true;
-};
