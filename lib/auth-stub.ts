3c1a36cb543512291cd649c54698b2e9a98ca8ca
Alina Abdullina <fianit8@gmail.com>
2024-10-15 16:49:11 +0500

    add dictionary for auth

--- lib/auth-stub.ts
+++ lib/auth-stub.ts
@@ -1,7 +1,14 @@
 import { deleteCookie, setCookie } from "cookies-next";
 
+const credentials = [
+  {email: "admin", password: "admin"},
+  {email: "a", password: "a"},
+];
+
 export const signInStub = (email: string, password: string): boolean => {
-  if (email === "a" && password === "b") {
+  const user = credentials.find((credential) => 
+    (credential.email === email && credential.password === password));
+  if (user) {
     setCookie("currentUser", email);
     return true;
   }
